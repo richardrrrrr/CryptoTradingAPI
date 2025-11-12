a66a924b179049f5e98e9b3cd2e42fc43c09d16f
BANKPRO\richard_li <richard_li@bankpro.com.tw>
2025-11-12 18:13:11 +0800

    feat(binance): create binance api

---
+++ CryptoTrading.Infrastructure/Binance/BinanceOption.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptoTrading.Infrastructure.Binance
+{
+    public class BinanceOptions
+    {
+        public string ApiKey { get; set; } = string.Empty;
+        public string ApiSecret { get; set; } = string.Empty;
+        public bool UseTestnet { get; set; } = false;
+    }
+}
+
+
---
+++ CryptoTrading.Infrastructure/Binance/BinanceService.cs
@@ -0,0 +1,15 @@
+﻿using Binance.Net;
+using Binance.Net.Clients;
+using Binance.Net.Enums;
+using Binance.Net.Objects;
+using CryptoExchange.Net.Authentication;
+using CryptoTrading.Infrastructure.Binance;
+using Microsoft.Extensions.Options;
+
+namespace CryptoTrading.Infrastructure.Binance
+{
+    public class BinanceService
+    {
+        private readonly BinanceRestClient _client;
+    }
+}
